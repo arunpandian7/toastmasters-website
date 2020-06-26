8b3a08947dddd0b305ccdf41635cb8a370ac0f8c
Arun Pandian R <smartboyarun56@gmail.com>
2020-06-26 15:50:37 +0530

    Site Meta Added

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,27 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: 'SOSC',
+    siteUrl: 'https://sosc.org.in',
+    events: {
+      title: 'SOSC - Events',
+      siteUrl: 'https://sosc.org.in/events',
+    },
+    team: {
+      title: 'SOSC - Team',
+      siteUrl: 'https://sosc.org.in/team',
+    },
+    blog: {
+      title: 'SOSC - Blog',
+      siteUrl: 'https://sosc.org.in/blog',
+    },
+    memberDetails: {
+      title: 'SOSC - Member Details',
+      siteUrl: 'https://sosc.org.in/member-details',
+    },
+  },
+  plugins: [
+    
+  ],
 }
