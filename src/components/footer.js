6903c57a51bb44e58ec20acd2ee0fa870c7a0803
Arun Pandian R <smartboyarun56@gmail.com>
2020-07-03 23:20:32 +0530

    Updated Credit Section

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,20 +5,16 @@ const Footer = ({ siteTitle }) => (
     <div className="container">
       <div className="info-area">
         <div className="column">
-          <a href="https://github.com/so-sc">GitHub</a>
           <a href="https://instagram.com/sosc.sahyadri">Instagram</a>
           <a href="https://linkedin.com/company/sosc-sahyadri">LinkedIn</a>
           <a href="https://twitter.com/sahyadri_osc">Twitter</a>
         </div>
+        
         <div className="column">
-          <a href="https://github.com/so-sc/code-of-conduct">Community Guidelines</a>
-          <a href="https://github.com/so-sc/code-of-conduct">Code of Conduct</a>
-        </div>
-        <div c lassName="column">
           <p>
-            <strong>KPRIET Toastmasters Club</strong><br/>
+            <strong>The Toastmasters Club,</strong><br/>
             KPR Institute of Engineering and Technology,<br/>
-            Arasur, Coimbatore IN<br/><br/>
+            Avinashi Road, Arasur, Coimbatore IN<br/><br/>
             toastmastersofficial7@gmail.com<br/>
             +91 76396 45032
           </p>
@@ -26,8 +22,19 @@ const Footer = ({ siteTitle }) => (
       </div>
     </div>
     <div className="copyright-area">
-      <p>KPRIET TMC © { new Date().getFullYear() }</p>
+      <div className="copyright-text">
+        <p>© { new Date().getFullYear() } KPRIET Toastmasters Club. All Rights Reserved </p>
+      </div>
+
+      <div className="credit-text">
+        <p>
+        Developed with ❤ by <a href="https://arunpandian7.github.io"> Arun Pandian R </a>
+        </p>
+      </div> 
+
+
     </div>
+    
   </footer>
 )
 
